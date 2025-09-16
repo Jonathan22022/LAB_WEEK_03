d010976afe30ffab59bb16e46b42516e4a1492b3
Jonathan22022 <jonathangautama0406@gmail.com>
2025-09-17 04:45:40 +0700

    Commit No. 2 – add coffee list & detail fragment
    Commit No. 2 – add coffee list & detail fragment

---
+++ app/src/main/java/com/example/lab_week_03/DetailFragment.kt
@@ -0,0 +1,82 @@
+package com.example.lab_week_03
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class DetailFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+    private val coffeeTitle: TextView?
+        get() = view?.findViewById(R.id.coffee_title)
+    private val coffeeDesc: TextView?
+        get() = view?.findViewById(R.id.coffee_desc)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_detail, container, false)
+
+        // Ambil TextView
+        val titleText: TextView = view.findViewById(R.id.coffee_title)
+        val descText: TextView = view.findViewById(R.id.coffee_desc)
+
+        // Set text manual dari string.xml
+        titleText.text = getString(R.string.affogato_title)
+        descText.text = getString(R.string.affogato_desc)
+
+        // Tambahkan log untuk debugging
+        Log.d("DetailFragment", "Title TextView found? ${titleText != null}")
+        Log.d("DetailFragment", "Desc TextView found? ${descText != null}")
+        Log.d("DetailFragment", "Title value = ${titleText.text}")
+        Log.d("DetailFragment", "Desc value = ${descText.text}")
+
+        return view
+    }
+
+    fun setCoffeeData(id: Int) {
+        when (id) {
+            R.id.affogato -> {
+                coffeeTitle?.text = getString(R.string.affogato_title)
+                coffeeDesc?.text = getString(R.string.affogato_desc)
+            }
+
+            R.id.americano -> {
+                coffeeTitle?.text = getString(R.string.americano_title)
+                coffeeDesc?.text = getString(R.string.americano_desc)
+            }
+
+            R.id.latte -> {
+                coffeeTitle?.text = getString(R.string.latte_title)
+                coffeeDesc?.text = getString(R.string.latte_desc)
+            }
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            DetailFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
